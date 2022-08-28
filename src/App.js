3e6d02b559ec57af74618c32fc20987a00eb0ab5
FedeBentolila <federico_bentolila@hotmail.com>
2022-08-28 11:16:01 -0300

    itemcount

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
       <NavBar/>
       </header>
-      <ItemListContainer greeting="Bienvenidos a la tienda de insumos para endoscopía digestiva"/>
+      <ItemListContainer greeting="Bienvenidos a la tienda de insumos para endoscopía digestiva"/>  
 
     </div>
   );
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,22 @@
 import './ItemListContainer.css';
+import ItemCount from '../ItemCount/ItemCount';
 
 const ItemListContainer =({greeting})=>{
+
+    const onAdd = (qty) => {
+        alert(`Agregaste ${qty} productos`);
+      };
+
     return (
         <div className="ItemListContainer">
 
             <div className="Greeting">
                 <h1>{greeting}</h1>
             </div>
+
+            <>
+            <ItemCount onAdd={onAdd} initial={1} stock={7} />
+            </>
 
         </div>
 
