d5f1d6ed853e633705cbfe9d9df9511485668685
FedeBentolila <federico_bentolila@hotmail.com>
2022-09-14 19:36:17 -0300

    cart

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -1,25 +1,28 @@
 import React, {useContext} from "react";
 import { CartContext } from "../../context/CartContext";
+import '../CartContainer/CartContainer.css';
 
 const CartContainer =()=>{
     const {productCartList, removeItem, clear} = useContext(CartContext)
     return (
         <div>
 
-            <div>
+            <div className="CartGlobal">
                 {productCartList.map(item=>(
                     <>
                     <p>{item.name} - {item.quantity} </p>
-                    <button onClick={()=>removeItem(item.id)}>eliminar producto</button>
+                    <button className="ButtonCart" onClick={()=>removeItem(item.id)}>eliminar producto</button>
                     </>
                 ))
 
                 }
-                <>
+                
                 {productCartList.length?(
-                  <button onClick={()=>clear()}>eliminar todos los productos</button>  
+                    <>
+                  <button className="ButtonCart" onClick={()=>clear()}>eliminar todos los productos</button> 
+                  </> 
                 ):("") }
-                </>
+                
 
 
             </div>
